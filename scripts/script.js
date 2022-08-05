4954231e81fc5f2a03db01583a128d2b44f5857d
JVPH <gyrospizzeria@protonmail.com>
2022-08-04 22:45:39 -0500

    Add three functions to play the game properly.

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,22 @@ function getComputerChoice(){
     const compChoice = options[Math.floor(Math.random()*options.length)];
     return compChoice;
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection.localeCompare(computerSelection.toString(), 'en', { sensitivity: 'base'}) === 0){
+        return 'Draw';
+    }else if(playerSelection.localeCompare('Rock', 'en', { sensitivity: 'base'}) === 0 && computerSelection.localeCompare('Paper') === 0){
+        return 'You Lose! Paper beats Rock';
+    }else if(playerSelection.localeCompare('Scissors', 'en', { sensitivity: 'base'}) === 0 && computerSelection.localeCompare('Rock') === 0){
+        return 'You Lose! Rock beats Scissors';
+    }else {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
+    }              
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        const playerChoice = prompt('Type your pick: ');
+        alert(playRound(playerChoice, getComputerChoice()));       
+    }
+}
