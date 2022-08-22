279946ec0a2889fa3fb1b63a3054bfc997d634c1
JVPH <gyrospizzeria@protonmail.com>
2022-08-22 09:58:43 -0500

    Update logic of playRound()

--- scripts/script.js
+++ scripts/script.js
@@ -7,17 +7,17 @@ buttons.forEach((button) => {
 });
 
 function getComputerChoice(){
-    const options = ['Rock', 'Paper', 'Scissors'];
+    const options = ['rock', 'paper', 'scissors'];
     const compChoice = options[Math.floor(Math.random()*options.length)];
     return compChoice;
 }
 
 function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection.toLowerCase){
+    if(playerSelection === computerSelection){
         return 'Draw';
-    }else if(playerSelection === 'rock' && computerSelection.toLowerCase === 'paper'){
+    }else if(playerSelection === 'rock' && computerSelection === 'paper'){
         return 'You Lose! Paper beats Rock';
-    }else if(playerSelection === 'scissors' && computerSelection.toLowerCase === 'rock'){
+    }else if(playerSelection === 'scissors' && computerSelection === 'rock'){
         return 'You Lose! Rock beats Scissors';
     }else {
         return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()} beats ${computerSelection}`;
